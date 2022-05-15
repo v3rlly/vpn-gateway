3a5106789d4ff0ea6fe5ca1442f83a61dce1d742
v3rlly <pablov3rlly@gmail.com>
2022-05-15 17:11:54 -0300

    Update vpn-init.sh

--- vpn-init.sh
+++ vpn-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 bash /etc/openvpn/iptables.sh &
 sleep 15
-sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-sudo bash /etc/openvpn/connect.sh &
+sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+bash /etc/openvpn/connect.sh &
 
 exit 0
